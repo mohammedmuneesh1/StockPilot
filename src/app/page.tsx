31db96d460cfbd0850b29bbd4f6aabc4aa4561c5
Mohammed Muneesh E K <mohammedmuneesh1@gmail.com>
2026-05-16 21:33:41 +0530

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-green-400">
+
+      <Button>
+        Click Me
+      </Button>
+
+
+    </div>
+  )
+}
+
+export default page
